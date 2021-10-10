fb672072d85dc14940802ca42967bd2b5248fc49
siavash sattari <siavash.sattari.75@gmail.com>
2021-10-10 19:33:12 +0330

    UI Class - Render User Profile

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 let github = new Github();
+let ui = new UI();
 
 // Search Input
 
@@ -11,13 +12,17 @@ searchInput.addEventListener("keyup", (e) => {
   if (username !== "") {
     // get profile form git
     github.getUser(username).then((data) => {
+        // console.log(data);
       if (!data.profile.message) {
         // show profile in webpage
+        ui.showProfile(data.profile)
       } else {
         // show alert
+        ui.showAlert("User not found...", "alert alert-danger");
       }
     });
   } else {
     // clear profile from webpage
+    ui.clearProfile()
   }
 });
